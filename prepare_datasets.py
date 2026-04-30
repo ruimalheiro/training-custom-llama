35d384112eb85c21f36faa78280639e430d0a51b
Rui Malheiro <rmpmalheiro@gmail.com>
2026-04-30 15:53:46 +0100

    supporting evals in the new structure

--- prepare_datasets.py
+++ prepare_datasets.py
@@ -5,7 +5,9 @@ from config import config
 from datasets_preparation import (
     prepare_pretraining_dataset,
     prepare_instruct_dataset,
-    prepare_dpo_dataset
+    prepare_dpo_dataset,
+    prepare_hellaswag_dataset,
+    prepare_winogrande_dataset
 )
 
 
@@ -17,16 +19,30 @@ def load_custom_dataset_mix(mix_file_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Datasets Preparation Script Options')
-    parser.add_argument('--pretraining', action='store_true', help='Prepare pretraining dataset')
-    parser.add_argument('--instruct', action='store_true', help='Prepare instruct (SFT) dataset')
-    parser.add_argument('--dpo', action='store_true', help='Prepare DPO (Direct Preference Optimization) dataset')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--hellaswag', action='store_true', help='Prepare HellaSwag eval dataset')
+    group.add_argument('--winogrande', action='store_true', help='Prepare WinoGrande eval dataset')
+    group.add_argument('--pretraining', action='store_true', help='Prepare pretraining dataset')
+    group.add_argument('--instruct', action='store_true', help='Prepare instruct (SFT) dataset')
+    group.add_argument('--dpo', action='store_true', help='Prepare DPO (Direct Preference Optimization) dataset')
+
     parser.add_argument('--mix-file', type=str, default=None, help='Path to custom mix file')
 
     args = parser.parse_args()
 
-    if args.pretraining:
-        prepare_pretraining_dataset(datasets_mix=load_custom_dataset_mix(args.mix_file))
+    if (args.hellaswag or args.winogrande) and args.mix_file:
+        parser.error('"--mix-file" is only supported for training datasets.')
+
+    datasets_mix = load_custom_dataset_mix(args.mix_file)
+
+    if args.hellaswag:
+        prepare_hellaswag_dataset()
+    elif args.winogrande:
+        prepare_winogrande_dataset()
+    elif args.pretraining:
+        prepare_pretraining_dataset(datasets_mix=datasets_mix)
     elif args.instruct:
-        prepare_instruct_dataset(datasets_mix=load_custom_dataset_mix(args.mix_file))
+        prepare_instruct_dataset(datasets_mix=datasets_mix)
     elif args.dpo:
-        prepare_dpo_dataset(datasets_mix=load_custom_dataset_mix(args.mix_file))
+        prepare_dpo_dataset(datasets_mix=datasets_mix)
